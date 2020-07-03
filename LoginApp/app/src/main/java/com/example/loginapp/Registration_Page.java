ef64c8e8ca5977ce59d2d62edce21a8055bed8f1
Vijay Kashyap Kompella <vijaykashyapkompella00@gmail.com>
2020-07-03 09:58:29 +0530

    Login App

---
+++ LoginApp/app/src/main/java/com/example/loginapp/Registration_Page.java
@@ -0,0 +1,14 @@
+package com.example.loginapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+class RegistrationPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration_page);
+    }
+}
