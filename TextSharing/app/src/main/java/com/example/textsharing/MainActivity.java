320b265596530008b24eb89b59d970edfef2d4a3
Vijay Kashyap Kompella <vijaykashyapkompella00@gmail.com>
2020-07-07 17:16:17 +0530

    Camera and Gallery App and Text Sharing App

---
+++ TextSharing/app/src/main/java/com/example/textsharing/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.textsharing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    Button bone;
+    EditText edittext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edittext = findViewById(R.id.edittext);
+        bone = findViewById(R.id.bone);
+
+        bone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String message = edittext.getText().toString();
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.putExtra(Intent.EXTRA_TEXT, message);
+                sendIntent.setType("text/plain");
+
+                Intent shareIntent = Intent.createChooser(sendIntent, null);
+                startActivity(shareIntent);
+
+            }
+        });
+    }
+}
